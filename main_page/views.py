6d18b6a4b8b56e65303c322fb264c7c8b563e753
aunlega <dbogak@mail.ru>
2021-03-21 16:55:15 +0600

    Version 1.0

--- main_page/views.py
+++ main_page/views.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 
 def main_page(request, *args, **kwargs):
-    return render(request, 'Home.html', {})
+    return render(request, 'index.html', {})
 
 
 def contacts(request):
